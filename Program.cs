49e71255932f597c886b5b9ef9712ce2aceb58ed
leonardorafaelli <leonardorafaelli@gmail.com>
2024-07-07 21:16:11 +0200

    feat: add pokemon data structure (studying DSs)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CSharpTicTacToe.ADT;
+
 Console.WriteLine("Hello, World!");
+
+
+Pokemon squirtle = new Pokemon();
+
+Console.WriteLine(squirtle.Name);
--- obj/Debug/net8.0/CSharpTicTacToe.AssemblyInfo.cs
+++ obj/Debug/net8.0/CSharpTicTacToe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpTicTacToe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cc6f5b4541bdb42241947333b4d11986afdff6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpTicTacToe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpTicTacToe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
